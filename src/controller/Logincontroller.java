1b687b491242bb60453ad3d25c41ccbe19f136e8
Yan <81289139+Yankiie@users.noreply.github.com>
2022-12-05 21:05:48 -0300

    Update Logincontroller.java

--- src/controller/Logincontroller.java
+++ src/controller/Logincontroller.java
@@ -1,74 +1,111 @@
 package controller;
 
+
+import java.io.FileInputStream;
+
 import javafx.application.Application;
 import javafx.event.EventHandler;
 import javafx.geometry.Insets;
+import javafx.scene.Group;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
 import javafx.scene.control.TextField;
+import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
 import javafx.scene.input.MouseEvent;
 import javafx.scene.layout.ColumnConstraints;
 import javafx.scene.layout.GridPane;
 import javafx.stage.Stage;
 
-public class Logincontroller extends Application {
-    @Override
-    public void start(Stage stg) throws Exception {
-        GridPane gp = new GridPane();
-        Scene scn = new Scene(gp, 600, 150);
 
-        Button btnLogin = new Button("Fazer Login");
-        Button btnPesquisa = new Button("N�o tem Login?");
+public class PerfilController extends Application {
+	@Override
+	public void start(final Stage stg) throws Exception {
+        final GridPane gp = new GridPane();
+        Scene scn = new Scene(gp, 900, 600);
 
-        Label lblNome = new Label("Usuario: ");
-        TextField txtNome = new TextField();
+        Button btnLogin = new Button("Aplicar");
+        
+        Button btnRedefinir = new Button("Redefinir Senha");
+        
+        Label lblNome = new Label("Nome: ");
 
-        Label lblTel = new Label("Senha: ");
-        TextField txtTel = new TextField();
+        Label lblEmail = new Label("Email: ");
+        
+        Label lblEndereco = new Label("Endereco: ");
 
+        Label lblTel = new Label("Telefone: ");
+        
+        Label lblSenha = new Label("Senha: ");
+        TextField txtSenha = new TextField();
+                      
+        Image image = new Image(new FileInputStream("C:\\Users\\yan\\Desktop\\17004.png"));  
+        
+        ImageView imageView = new ImageView(image); 
+        
+        imageView.setX(50); 
+        imageView.setY(25); 
+        
+        imageView.setFitHeight(150); 
+        imageView.setFitWidth(150); 
+        
+        imageView.setPreserveRatio(true);          
+ 
+        Group root = new Group(imageView);
+   
+        
+        gp.add(root, 0, 0);
+        
         gp.add(lblNome, 0,1);
-        gp.add(txtNome, 1,1);
-        gp.add(lblTel, 0,2);
-        gp.add(txtTel, 1,2);
-        gp.add(btnLogin, 0, 3);
-        gp.add(btnPesquisa, 1, 3);
 
+        gp.add(lblEmail, 0,2);
+
+        gp.add(lblEndereco, 0,3);
+
+        gp.add(lblTel, 0,4);
+
+        gp.add(lblSenha, 0,5);
+        gp.add(txtSenha, 1,5);      
+               
+        gp.add(btnRedefinir, 1, 7);
+        
+        gp.add(btnLogin, 0, 12);
 
 
         ColumnConstraints col1 = new ColumnConstraints();
         ColumnConstraints col2 = new ColumnConstraints();
         col1.setPercentWidth(15);
-        col2.setPercentWidth(50);
+        col2.setPercentWidth(25);
 
         gp.getColumnConstraints().addAll(col1, col2);
         gp.setVgap(10);
         gp.setPadding(new Insets(15));
 
-        stg.setResizable(true);
+        stg.setResizable(false);
         stg.setScene(scn);
-        stg.setTitle("Login Hotelaria");
+        stg.setTitle("Meu Perfil");
         stg.show();
         
-        btnLogin.addEventHandler(MouseEvent.MOUSE_CLICKED, new EventHandler<MouseEvent>() {
-            @Override
-            public void handle(MouseEvent event) {
-            	stg.setTitle("Login Efetuado com Sucesso");
-            }
-        });
+        btnRedefinir.addEventHandler(MouseEvent.MOUSE_CLICKED, new EventHandler<MouseEvent>() {
+        public void handle(MouseEvent event) {
+        	stg.setTitle("Redefinição de Senha");
+        	
+        	Label lblSenhaNova = new Label("Senha Nova: ");
+        	TextField txtSenhaNova = new TextField();
+            
+            gp.add(lblSenhaNova, 0, 6);
+            gp.add(txtSenhaNova, 1, 6);
+                            	        	        	
+        } 
+    
+});
         
-        btnPesquisa.addEventHandler(MouseEvent.MOUSE_CLICKED, new EventHandler<MouseEvent>() {
-            @Override
-            public void handle(MouseEvent event) {
-            	stg.setTitle("Cadastro");
-            }
-        });
+
         
     }
     
-    
-    
     public static void main(String[] args) {
-		Application.launch(Logincontroller.class, args);
+		Application.launch(PerfilController.class, args);
 	}
 }
